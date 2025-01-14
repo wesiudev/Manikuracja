1fdbde29ab7066bf3835e021811455dd36818526
wesselpawel <wesselpawel@wp.pl>
2025-01-14 19:43:30 +0100

    +sitemap

---
+++ app/cities/route.ts
@@ -0,0 +1,8 @@
+import data from "polskie-miejscowosci";
+import { createLinkFromText } from "@/utils/createLinkFromText";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const cities = data.map((city) => ({ Name: createLinkFromText(city.Name) }));
+  return NextResponse.json(cities);
+}
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,17 +1,16 @@
 import { createLinkFromText } from "@/utils/createLinkFromText";
 import type { MetadataRoute } from "next";
-import data from "polskie-miejscowosci";
 
 // Helper function to generate URLs for services and cities
 function generateUrls(
-  services: string[]
+  services: string[],
+  cities: string[]
 ): { url: string; lastModified: Date }[] {
-  const linkCities = data.map((city) => createLinkFromText(city.Name));
   const linkServices = services.map((service) => createLinkFromText(service));
   const url = process.env.NEXT_PUBLIC_URL;
   let idCounter = 1;
   return linkServices.flatMap((service) =>
-    linkCities.map((city) => ({
+    cities.map((city) => ({
       id: idCounter++,
       url: `${url}/${service}/${city}`,
       lastModified: new Date(),
@@ -27,11 +26,19 @@ async function fetchServices(): Promise<string[]> {
   }
   return response.json();
 }
+async function fetchCities(): Promise<string[]> {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/cities`);
+  if (!response.ok) {
+    throw new Error("Failed to fetch services data.");
+  }
+  return response.json();
+}
 
 // Generate all sitemaps
 export async function generateSitemaps() {
+  const cities = await fetchCities();
   const services = await fetchServices();
-  return generateUrls(services);
+  return generateUrls(services, cities);
 }
 
 // Generate paginated sitemap
@@ -41,11 +48,10 @@ export default async function sitemap({
   id: number;
 }): Promise<MetadataRoute.Sitemap> {
   const services = await fetchServices();
-
+  const cities = await fetchCities();
   // Google's limit is 50,000 URLs per sitemap
-  const urls = generateUrls(services);
-  const start = id * 5000;
-  const end = start + 5000;
-
+  const urls = generateUrls(services, cities);
+  const start = id * 50000;
+  const end = start + 50000;
   return urls.slice(start, end);
 }
