93f834a9687020f8dc3a6e1a794bf79a8d9352ab
wesselpawel <wesselpawel@wp.pl>
2025-01-24 20:41:47 +0100

    user fetch

--- utils/fetchUser.ts
+++ utils/fetchUser.ts
@@ -2,7 +2,6 @@
 export async function fetchUser(uid: string) {
   const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/users/${uid}`, {
     method: "POST",
-    body: JSON.stringify({ secret: process.env.SECRET }),
   });
   const user = await response.json();
   return user;
