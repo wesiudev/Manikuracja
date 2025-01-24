4f6be1d9dce88efc2d8dc382e6788cf8f5246939
wesselpawel <wesselpawel@wp.pl>
2025-01-24 20:55:21 +0100

    success page

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function SuccessPage() {
   const router = useRouter();
